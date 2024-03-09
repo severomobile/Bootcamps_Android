7744afca0cbcaef8166b2e699ffe82ef093daf73
severomobile <driveriverajoseluciano@gmail.com>
2024-03-09 20:04:57 -0300

    Agregando ejercicio imprimiendo pares

---
+++ Ejercicios Algoritmos/14_Clase Ejercicio 2.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int beginning = 1;
+        int end = 100;
+
+        for (int i = beginning; i <= end; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
