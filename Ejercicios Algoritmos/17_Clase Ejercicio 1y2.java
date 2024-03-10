5856143a37faabf0af6b247c0d6530014ca4b075
severomobile <driveriverajoseluciano@gmail.com>
2024-03-09 23:42:21 -0300

    Agregado ejercicio variables en java

---
+++ Ejercicios Algoritmos/17_Clase Ejercicio 1y2.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        char identificador = 3;
+        boolean estudiante = true;
+        String apellido = "Rivera";
+
+        byte numero = 5;
+        short numero2 = 5;
+        int numero3 = 5;
+        long numero4 = 5;
+        float numero5 = 5.4F;
+        double numero6 = 5.5;
+
+        System.out.println(identificador);
+        System.out.println(estudiante);
+        System.out.println("José "+apellido);
+
+        System.out.println(numero);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+        System.out.println(numero5);
+        System.out.println(numero6);
+    }
+}
